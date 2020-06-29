5383eba4f7b82d26fb93a7bf40a7e46b445eb007
Shalini DC <shalini.1325@gmail.com>
2020-06-28 23:26:20 -0700

    Youtube search and video player

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Grid } from '@material-ui/core'
+
+import SearchBar  from './components/SearchBar';
+import VideoDetail  from './components/VideoDetail'
+import youtube from "./api/youtube";
+
+class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo : null
+        }
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key:'AIzaSyCabOYuQdAF-tSlyB5XqnRnzYguV6CkBbI',
+                q: searchTerm,
+            }
+        });
+        this.setState({
+            videos: response.data.items, selectedVideo: response.data.items[1]
+        });
+    }
+
+    render() {
+        return (
+            <Grid container spacing={10} justify='center'>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={10}>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/*VIDEO LIST*/}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default App;
